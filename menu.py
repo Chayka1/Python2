e5fd173bc23925df698307508aaeac5c5b9c0e55
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2022-11-25 17:11:33 +0200

    1

--- menu.py
+++ menu.py
@@ -245,7 +245,7 @@ while True:
                         break
 
             elif exercise_2 == '9':
-                print()
+                print(task_2_9())
                 print('-----------------------------------------------')
                 action = input('1.Выход из меню\n' + '0.Назад\n')
                 while True:
